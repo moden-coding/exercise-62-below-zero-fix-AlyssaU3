f253c7b712f4b1a1409beb02e3251684c98c1036
AlyssaU3 <alys4321c@gmail.com>
2025-10-28 13:34:10 -0400

    62 done

--- src/below_zero.py
+++ src/below_zero.py
@@ -3,7 +3,13 @@
 import pandas as pd
 
 def below_zero():
-    pass
+    below_zero=[]
+    zero= pd.read_csv("src/kumpula-weather-2017.csv")
+    days= zero["Air temperature (degC)"]
+    for day in days:
+        if day<0:
+            below_zero.append(day)
+    return len(below_zero)
 
 def main():
     result = below_zero()
